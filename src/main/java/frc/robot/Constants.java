8bda4baaeba509d040b00d4d9f9fe39a07ed26fe
UHDbits <uhdbits@duck.com>
2024-02-21 13:00:08 -0700

    Update Constants comment
    Meant to update this in the last commit, accidently didn't select it.
    
    Signed-off-by: UHDbits <uhdbits@duck.com>

--- src/main/java/frc/robot/Constants.java
+++ src/main/java/frc/robot/Constants.java
@@ -31,7 +31,7 @@ public final class Constants {
     public static final Boolean kPhysicalSimulation = false;
     /** Boolean that represents whether or not extra vision debug information (specifically creating a Field2d for each camera) should be enabled. This can be intensive, so when not in use, it should be disabled. */
     public static final Boolean kExtraVisionDebug = false;
-    /** Boolean that represents whether or not to use vision pose estimation in a simulation. This will allow testing of the vision subsystem, but will also make the testing of other robot components annoying, as the vision pose isn't always 100% right. */
+    /** Boolean that represents whether or not to use vision pose estimation in a simulation. This will allow testing of the vision subsystem, but will also make the testing of other robot components annoying, as the vision pose isn't always 100% right (and is also potentially slow when being simulated). */
     public static final Boolean kVisionPoseEstimationSimulation = true;
     public static final AprilTagFieldLayout kFieldLayout =
         AprilTagFields.k2024Crescendo.loadAprilTagLayoutField();
