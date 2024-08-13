43750c3ef5d0a5a901ff961b0a6c297015c1b461
Ashton A <uhdbits@duck.com>
2024-08-12 19:03:21 -0700

    Add new speaker auto shoot command
    This commit adds a brand new auto shoot command that is meant for the arm in a static position, and hopefully this will allow us to shoot a lot faster in both autonomous and teleop. This commit also tunes the swerve power limits and ramp rates, and creates the file for a new Center 4 Note auto.
    
    Signed-off-by: Ashton A <uhdbits@duck.com>

---
+++ src/main/java/frc/robot/commands/ShooterOnly/AutoSpeakerShoot.java
@@ -0,0 +1,63 @@
+// Copyright (c) FIRST and other WPILib contributors.
+// Open Source Software; you can modify and/or share it under the terms of
+// the WPILib BSD license file in the root directory of this project.
+
+package frc.robot.commands.ShooterOnly;
+
+import com.revrobotics.CANSparkBase;
+import com.revrobotics.SparkPIDController;
+
+import edu.wpi.first.math.filter.Debouncer;
+import edu.wpi.first.math.filter.Debouncer.DebounceType;
+import edu.wpi.first.wpilibj2.command.Command;
+import frc.robot.Constants.SpeedConstants;
+import frc.robot.RobotContainer;
+import frc.robot.subsystems.PrimerSubsystem;
+import frc.robot.subsystems.ShooterSubsystem;
+
+public class AutoSpeakerShoot extends Command {
+  private final ShooterSubsystem shooter = RobotContainer.m_shooterSubsystem;
+  private final PrimerSubsystem primer = RobotContainer.m_primerSubsystem;
+
+  private Debouncer speakerDebouncer = new Debouncer(0.1, DebounceType.kBoth);
+
+  private final SparkPIDController leftPIDController = shooter.leftShooter.getPIDController();
+  private final SparkPIDController rightPIDController = shooter.rightShooter.getPIDController();
+
+  private boolean noteShooting = false;
+  /** Creates a new SpeakerShoot. */
+  public AutoSpeakerShoot() {
+    // Use addRequirements() here to declare subsystem dependencies.
+    addRequirements(shooter, primer);
+  }
+
+  // Called when the command is initially scheduled.
+  @Override
+  public void initialize() {
+    noteShooting = false;
+  }
+
+  // Called every time the scheduler runs while the command is scheduled.
+  @Override
+  public void execute() {
+    double currentShooterVelocity = shooter.getAverageVelocity();
+
+    // Set speed of flywheels.
+    leftPIDController.setReference(4000, CANSparkBase.ControlType.kVelocity);
+    rightPIDController.setReference(-4000, CANSparkBase.ControlType.kVelocity);
+    
+    if (currentShooterVelocity > 2000 && RobotContainer.checkIntersection() && RobotContainer.getRobotPointedToSpeaker()) {
+      primer.setSpeed(SpeedConstants.kPrime);
+    };
+  }
+
+  // Called once the command ends or is interrupted.
+  @Override
+  public void end(boolean interrupted) {}
+
+  // Returns true when the command should end.
+  @Override
+  public boolean isFinished() {
+    return false;
+  }
+}
